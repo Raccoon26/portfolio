2ea879366043562d05341985b0a71d234ff33f7d
Raccoon26 <33raccoon33@gmail.com>
2024-02-01 10:41:30 +0900

    Development controller

--- src/main/kotlin/com/raccoon/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/raccoon/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,15 +1,42 @@
 package com.raccoon.portfolio.presentation.controller
 
+import com.raccoon.portfolio.presentation.dto.IntroductionDTO
+import com.raccoon.portfolio.presentation.dto.LinkDTO
+import com.raccoon.portfolio.presentation.dto.ProjectDTO
+import com.raccoon.portfolio.presentation.dto.ResumeDTO
+import com.raccoon.portfolio.presentation.service.PresentationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
 
-class PresentationApiController {
     @GetMapping("/test")
-    fun test(): String{
+    fun test(): String {
         return "OK"
     }
+
+    @GetMapping("/v1/introductions")
+    fun getIntroductions(): List<IntroductionDTO> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/links")
+    fun getLinks(): List<LinkDTO> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO {
+        return presentationService.getResume()
+    }
+
+    fun getProjects(): List<ProjectDTO> {
+        return presentationService.getProjects()
+    }
+
 }
